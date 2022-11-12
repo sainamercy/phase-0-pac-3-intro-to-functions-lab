92d187bf8aab43b6c4bfb5f6f6780cc80fa6f7a1
sainamercy <sainamercy@gmail.com>
2022-11-12 15:24:49 +0300

    assignment done

---
+++ index.js
@@ -0,0 +1,37 @@
+function shout(string) {
+    return string.toUpperCase()
+}
+
+console.log(shout("hello"))
+
+function whisper(string){
+    return string.toLowerCase()
+}
+
+console.log(whisper("Hello"));
+
+function logShout(string){
+    console.log(string.toUpperCase());
+}
+
+logShout("hello")
+
+function logWhisper(string){
+    console.log(string.toLowerCase());
+}
+
+logWhisper("HELLO")
+
+function sayHiToHeadphonedRoommate(string){
+    if (string=== string.toLowerCase()){
+        return "I can't hear you!"
+    }
+    else if (string === string.toUpperCase()){
+        return "YES INDEED!" 
+    }
+    else if (string === "Let's have dinner together!"){
+        return "I would love to!"
+    }
+}
+
+console.log(sayHiToHeadphonedRoommate("Let's have dinner together!"))
